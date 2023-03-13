ca1aa9aeeb7d9436643a4d0e93e6319b83beb016
Roman Bessouat <labess40@macbook-air-de-roman.home>
2023-03-13 21:25:40 +0100

    change folder organization

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import SimpleButton from './simpleButton';
-import StateExample from './stateChange';
-import StateAlignExample from './stateChangeAlign';
-
-const App = () => {
-  return <StateAlignExample />;
-};
-
-export default App;
